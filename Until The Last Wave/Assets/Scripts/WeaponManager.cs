55c129e545c729bf7415e9533945c5376dd716d8
Utku Gezensoy <utkugezensoy@gmail.com>
2025-05-13 17:22:10 +0300

    Gun Sight Aim Completed
    + Sight Aim with Camera Movement
    + Crosshair situations

--- Until The Last Wave/Assets/Scripts/WeaponManager.cs
+++ Until The Last Wave/Assets/Scripts/WeaponManager.cs
@@ -4,6 +4,7 @@ public enum WeaponType { Pistol, Shotgun, Rifle }
 
 public class WeaponManager : MonoBehaviour
 {
+    [Header("Weapon Objects")]
     public GameObject pistolObject;
     public GameObject shotgunObject;
     public GameObject rifleObject;
@@ -12,16 +13,29 @@ public class WeaponManager : MonoBehaviour
     public Animator pistolAnimator;
     public Animator shotgunAnimator;
     public Animator rifleAnimator;
+
+    [Header("Camera Reference")]
+    public Transform cameraTransform;
+
+    [Header("ADS Timing")]
+    public float adsLockDelay = 0.5f;
+
+    private float adsTimer = 0f;
     private bool isAiming = false;
+    private bool hasLockedFinalTransform = false;
+
+    private Vector3 localOffsetPos;
+    private Quaternion localOffsetRot;
+
+    public GameObject crosshairGameObject;
 
     public WeaponType CurrentWeapon { get; private set; }
 
     void Start()
     {
-        EquipWeapon(WeaponType.Pistol); 
+        EquipWeapon(WeaponType.Pistol);
     }
 
-    
     public void EquipWeapon(WeaponType weaponType)
     {
         CurrentWeapon = weaponType;
@@ -34,21 +48,69 @@ public class WeaponManager : MonoBehaviour
     public void ToggleAim()
     {
         isAiming = !isAiming;
+        hasLockedFinalTransform = false;
+        adsTimer = 0f;
 
-        switch (CurrentWeapon)
+        Animator anim = GetCurrentAnimator();
+        anim?.SetBool("IsAiming", isAiming);
+
+        if (crosshairGameObject != null)
+            crosshairGameObject.SetActive(!isAiming);
+    }
+
+    void Update()
+    {
+        if (isAiming && !hasLockedFinalTransform)
         {
-            case WeaponType.Pistol:
-                pistolAnimator?.SetBool("IsAiming", isAiming);
-                break;
-            case WeaponType.Shotgun:
-                shotgunAnimator?.SetBool("IsAiming", isAiming);
-                break;
-            case WeaponType.Rifle:
-                rifleAnimator?.SetBool("IsAiming", isAiming);
-                break;
+            adsTimer += Time.deltaTime;
+            if (adsTimer >= adsLockDelay)
+            {
+                Transform weaponTransform = GetCurrentWeaponTransform();
+                if (weaponTransform != null && cameraTransform != null)
+                {
+                    localOffsetPos = Quaternion.Inverse(cameraTransform.rotation) * (weaponTransform.position - cameraTransform.position);
+                    localOffsetRot = Quaternion.Inverse(cameraTransform.rotation) * weaponTransform.rotation;
+                    hasLockedFinalTransform = true;
+                }
+            }
         }
     }
 
+    void LateUpdate()
+    {
+        if (isAiming && hasLockedFinalTransform)
+        {
+            Transform weaponTransform = GetCurrentWeaponTransform();
+            if (weaponTransform != null && cameraTransform != null)
+            {
+                weaponTransform.position = cameraTransform.position + cameraTransform.rotation * localOffsetPos;
+                weaponTransform.rotation = cameraTransform.rotation * localOffsetRot;
 
+                // Debug çizgi: silahýn yönü nereye bakýyor
+                Debug.DrawRay(weaponTransform.position, weaponTransform.forward * 10f, Color.green);
+            }
+        }
+    }
 
+    private Transform GetCurrentWeaponTransform()
+    {
+        return CurrentWeapon switch
+        {
+            WeaponType.Pistol => pistolObject.transform,
+            WeaponType.Shotgun => shotgunObject.transform,
+            WeaponType.Rifle => rifleObject.transform,
+            _ => null
+        };
+    }
+
+    private Animator GetCurrentAnimator()
+    {
+        return CurrentWeapon switch
+        {
+            WeaponType.Pistol => pistolAnimator,
+            WeaponType.Shotgun => shotgunAnimator,
+            WeaponType.Rifle => rifleAnimator,
+            _ => null
+        };
+    }
 }
